668ceb40b212fb0222c6dd442eedc3636f008c55
Ivo Bernardo Silva Capelo <ivo.capelo@gmail.com>
2014-11-22 21:25:56 +0000

    map enabled

--- Sweeper/source/game/Game.hx
+++ Sweeper/source/game/Game.hx
@@ -33,7 +33,7 @@ class Game extends FlxState {
     private var gridH:Int;
     public var _grid:Array<Array<Tile>>;
     private var _level:Level;
-	private var _levels : Array<Level> = [];//[new Level1(), new Level2() ];
+	private var _levels : Array<Level> = [new Level1(), new Level2() ];
 	private var _levelIndex : Int = 0;
 
     private var numberGrid:Array<Array<Int>>;
